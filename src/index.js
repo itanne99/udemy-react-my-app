c87f9bd732b2db5e77c6fdd45ce02039ee7d1a79
Ido Tanne <idovader@gmail.com>
2022-01-06 11:14:39 -0500

    React App Update (my-app)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+// Import React and ReactDOM Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Create React component
+const App = function () {
+    const buttonText = "Click me!";
+
+    return (
+        <div>
+            <label className="label" for="name">Enter Name:</label>
+            <input id="name" type="text"/>
+            <button style={{ backgroundColor: 'blue', color: 'white'}}> {buttonText} </button>
+
+        </div>
+    );
+};
+
+// Take the react component and show it on the screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App/>,
+    document.querySelector('#root')
+)
